c2da7eea718cfb4f25fb2065f033cc4a1147a7f0
Joel Wetzell <jwetzell@yahoo.com>
2024-10-03 21:24:10 -0500

    add decode to example

--- examples/osc-message.cjs
+++ examples/osc-message.cjs
@@ -1,5 +1,8 @@
 const osc = require('../dist/index.js')
 
-const buffer = osc.messageToBuffer({address:'/hello',args:[{type: 's', value: 'arg1'}]})
+const buffer = osc.messageToBuffer({address:'/hello1',args:[{type: 's', value: 'arg1'}]})
 
 console.log(buffer)
+const message = osc.messageFromBuffer(buffer)
+
+console.log(message)
