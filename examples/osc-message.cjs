eea5033147f1921d93d4428f6a1100899fd3e6d7
Joel Wetzell <jwetzell@yahoo.com>
2024-10-07 12:59:20 -0500

    remove example

--- examples/osc-message.cjs
+++
@@ -1,8 +0,0 @@
-const osc = require('../dist/index.js')
-
-const buffer = osc.messageToBuffer({address:'/hello1',args:[{type: 's', value: 'arg1'}]})
-
-console.log(buffer)
-const message = osc.messageFromBuffer(buffer)
-
-console.log(message)
