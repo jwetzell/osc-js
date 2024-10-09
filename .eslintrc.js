7b681be03333a0dbc729248003eb82e0453266f7
Joel Wetzell <jwetzell@yahoo.com>
2024-10-09 17:28:49 -0500

    bring in sendosc and convert to npm workspaces

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+      node: true,
+      commonjs: true,
+      es2021: true,
+    },
+    extends: ['airbnb', 'prettier'],
+    parserOptions: {
+      ecmaVersion: 'latest',
+    },
+    ignorePatterns: ["**/node_modules", "**/dist"]
+  };
+  
