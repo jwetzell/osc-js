1d7de95ce3fd10db4002a55e7d5588d6f89d857b
Joel Wetzell <jwetzell@yahoo.com>
2025-01-20 10:22:24 -0600

    switch eslint to new style

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['packages/**/*.ts']},
+  { ignores: ['**/*.test.js','eslint.config.mjs','examples/*'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettier,
+];
