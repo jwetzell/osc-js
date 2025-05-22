b1e74c9eae2ef576c097b209b0f49938bf2cffe1
Joel Wetzell <jwetzell@yahoo.com>
2025-05-21 20:09:31 -0500

    add slip decoding to readosc

--- apps/readosc/src/main.js
+++ apps/readosc/src/main.js
@@ -1,23 +1,37 @@
 #!/usr/bin/env node
 
-const { program } = require('commander');
+const { program, Option } = require('commander');
 const osc = require('@jwetzell/osc');
 const packageInfo = require('../package.json');
+const slip = require('slip');
 
 program.name(packageInfo.name);
 program.version(packageInfo.version);
 program.description('simple util to read osc packets from stdin');
-program.action(() => {
+program.addOption(new Option('--slip', 'slip encode message').default(false));
+program.action((options) => {
+  const slipDecoder = new slip.Decoder({
+    onMessage: (msg) =>{
+      const [message, bytesAfterMessage] = osc.messageFromBuffer(msg);
+      printOSCMessage(message)
+    },
+    maxMessageSize: 209715200,
+    bufferSize: 2048
+  });
   process.stdin.on('data', (data) => {
     try {
-      let remainingBytes = data;
-      while (remainingBytes.length > 0) {
-        try {
-          const [message, bytesAfterMessage] = osc.messageFromBuffer(remainingBytes);
-          remainingBytes = bytesAfterMessage;
-          console.log(JSON.stringify(message));
-        } catch (error) {
-          console.error({ error: error.toString() });
+      if (options.slip){
+        slipDecoder.decode(data)
+      } else {
+        let remainingBytes = data;
+        while (remainingBytes.length > 0) {
+          try {
+            const [message, bytesAfterMessage] = osc.messageFromBuffer(remainingBytes);
+            remainingBytes = bytesAfterMessage;
+            printOSCMessage(message)
+          } catch (error) {
+            console.error({ error: error.toString() });
+          }
         }
       }
     } catch (error) {
@@ -26,3 +40,7 @@ program.action(() => {
   });
 });
 program.parse();
+
+function printOSCMessage(oscMessage) {
+  console.log(JSON.stringify(oscMessage));
+}
